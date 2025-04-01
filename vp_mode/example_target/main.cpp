544a4e9728a9ca8373d40263a1e7db7fa18cd639
Jonaswinz <jonas.winzer@rwth-aachen.de>
2025-04-01 11:17:13 +0200

    Adding license.

--- vp_mode/example_target/main.cpp
+++ vp_mode/example_target/main.cpp
@@ -1,26 +1,21 @@
 /*
-Copyright 2018 Embedded Microprocessor Benchmark Consortium (EEMBC)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Original Author: Shay Gal-on
-*/
-
-/* File: core_main.c
-        This file contains the framework to acquire a block of memory, seed
-   initial parameters, tun t he benchmark and report the results.
-*/
-
+ * Copyright (C) 2025 ICE RWTH-Aachen
+ *
+ * This file is part of AFL++ VP-Mode.
+ *
+ * AFL++ VP-Mode is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * AFL++ VP-Mode is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AFL++ VP-Mode. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <string.h>
 #include <stdint.h>
@@ -58,7 +53,7 @@ int my_strcmp(const char *str1, const char *str2) {
 void main()
 {
     int i = 0;
-    char str[] = "password";
+    char str[] = "pass";
     char read_str[MAX];
     char read_c;
 
